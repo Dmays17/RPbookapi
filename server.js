3a8894da13b64dcb666b2355feff0cdd06481a94
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-10-10 14:26:21 -0500

    api

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose= require('mongoose')
+const morgan=require('morgan')
+//const path=require('path')
+require('dotenv').config()
+const app = express()
+const routes=require('./routes/api')
+const PORT = process.env.PORT ||8080
+const cors=require('cors')
+
+//MiddleWare
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
+
+
+
+app.use(morgan('tiny'))
+
+app.get('/',(req,res)=>{
+  res.send('Welcome to RPBOOK Api')
+})
+app.use('/api',routes)
+
+app.get('*',(req,res)=>{
+  res.send('404')
+})
+
+app.listen(PORT,
+    console.log(`Server on ${PORT}`))
